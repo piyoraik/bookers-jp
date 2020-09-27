40d92572466b385268feb4ba8bbb7d8a6ae9f0b5
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-09-27 14:18:37 +0900

    github actions

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+$(function () {
+  $(document).on('turbolinks:load', () => {
+    $('#user_postal_code').jpostal({
+      postcode: [
+        '#user_postal_code'
+      ],
+      address: {
+        "#user_prefecture_code": "%3",
+        "#user_city": "%4%5",
+        "#user_street": "%6%7"
+      }
+    });
+  });
+});
---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+  attachment :profile_image, destroy: false
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+  validates :postal_code, presence: true
+  validates :prefecture_code, presence: true
+  validates :city, presence: true 
+  validates :street, presence: true 
+  
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+end
